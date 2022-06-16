1972d5e520bb225f99aee371af80f8f7b158bc46
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-16 10:28:27 -0300

    Add project files.

---
+++ DateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current Date & Time is:  "+DateTime.Now);
+
+            Console.WriteLine("Enter a number of hours");
+            int passHours = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("In " + passHours + " hours it will be " + DateTime.Now.AddHours(passHours));
+            Console.ReadLine();
+
+
+        }
+    }
+}
